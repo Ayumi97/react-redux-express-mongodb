a14855bc096a44070af8680152529d3fef71df91
yangy <yangy_9710@sina.com>
2019-09-06 17:36:39 +0800

    个人中心信息展示2

--- src/component/user/User.js
+++ src/component/user/User.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Result, List, WhiteSpace} from 'antd-mobile';
+
 
 @connect(
     state=>state.user
@@ -7,12 +9,29 @@ import {connect} from 'react-redux';
 class User extends React.Component{
     
     render(){
-        console.log(this.props)
-        return(
+        const props = this.props;
+        const Item = List.Item;
+        const Brief = List.Item.Brief;
+        return props.user ? (
             <div>
-
+                <Result 
+                    img={<img src={require(`../img/${props.avatar}.png`)} style={{width:50}} alt='' />}
+                    title={props.user}
+                    message={props.type==='boss'?props.company:null}
+                />
+                <List renderHeader={()=>'简介'}>
+                    <Item multipleLine wrap>
+                        {props.title}
+                        {props.desc.split('\n').map(v=><Brief key={v}>{v}</Brief>)}
+                        {props.money?<Brief>薪资：{props.money}</Brief> : null}
+                    </Item>
+                </List>
+                <WhiteSpace></WhiteSpace>
+                <List>
+                    <Item>退出登录</Item>
+                </List>
             </div>
-        )
+        ) : null;
     }
 }
 
