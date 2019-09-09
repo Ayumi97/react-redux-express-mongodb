8dca4f4a400847ad14b5b98398ac8d586c3eaa67
yangy <yangy_9710@sina.com>
2019-09-09 11:07:22 +0800

    注销-清除cookie登录状态

--- src/component/user/User.js
+++ src/component/user/User.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Result, List, WhiteSpace} from 'antd-mobile';
-
+import {Result, List, WhiteSpace, Modal} from 'antd-mobile';
+import './user.css';
+import browserCookie from 'browser-cookies';
 
 @connect(
     state=>state.user
 )
 class User extends React.Component{
+   constructor(props){
+       super(props);
+       this.logout = this.logout.bind(this)
+   }
+    logout(){       
+        const alert = Modal.alert;
+        alert('注销', '确定退出登录吗？', [
+            {text:'取消', onPress:() => console.log('cancel') },
+            { text:'确认', onPress: () => {
+                browserCookie.erase('userId');
+                // this.props.logoutSubmit()
+            } }
+        ])
+    }
     
     render(){
         const props = this.props;
         const Item = List.Item;
-        const Brief = List.Item.Brief;
+        const Brief = Item.Brief;
         return props.user ? (
             <div>
                 <Result 
                     img={<img src={require(`../img/${props.avatar}.png`)} style={{width:50}} alt='' />}
                     title={props.user}
-                    message={props.type==='boss'?props.company:null}
+                    message={props.type==='boss' ? props.company : null}
                 />
                 <List renderHeader={()=>'简介'}>
-                    <Item multipleLine wrap>
+                    <Item multipleLine>
                         {props.title}
                         {props.desc.split('\n').map(v=><Brief key={v}>{v}</Brief>)}
-                        {props.money?<Brief>薪资：{props.money}</Brief> : null}
+                        {props.money ? <Brief>薪资：{props.money}</Brief> : null}
                     </Item>
                 </List>
                 <WhiteSpace></WhiteSpace>
-                <List>
-                    <Item>退出登录</Item>
+                <List style={{zIndex:'100'}}>
+                    <Item onClick={this.logout}>
+                        退出登录
+                    </Item>
                 </List>
             </div>
         ) : null;
