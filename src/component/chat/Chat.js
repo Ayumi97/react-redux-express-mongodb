cd256893e98c16b0d54d2d82fe0d2e9fce293e5e
yangy <yangy_9710@sina.com>
2019-09-09 15:34:24 +0800

    socket.io前后端联通

--- src/component/chat/Chat.js
+++ src/component/chat/Chat.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class Chat extends React.Component{
     
-
+    componentDidMount(){
+        const socket = io('ws://localhost:9093')
+    }
 
     render(){
         return(
