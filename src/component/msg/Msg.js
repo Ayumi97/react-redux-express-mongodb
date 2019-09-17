f94bf1354a7fca48cd946d83124ea065f71480bd
yangy <yangy_9710@sina.com>
2019-09-17 09:38:02 +0800

    聊天列表展示

--- src/component/msg/Msg.js
+++ src/component/msg/Msg.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {List} from 'antd-mobile';
 
 
 @connect(
@@ -7,19 +8,45 @@ import {connect} from 'react-redux';
 )
 class Msg extends React.Component{
 
+    getLast(arr){
+        return arr[arr.length-1];
+    }
 
     render(){
-        console.log(this.props)
+        // if(!this.props.chat.chatmsg) {
+        //     return [];
+        // }
+        // console.log(this.props)
         // 按照聊天用户分组，根据chatid
         const msgGroup = {};
         this.props.chat.chatmsg.forEach(v=>{
             msgGroup[v.chatid] = msgGroup[v.chatid] || [];
             msgGroup[v.chatid].push(v);
         })
-        console.log(msgGroup)
+        // console.log(msgGroup)
+        const chatList = Object.values(msgGroup);
+        const Item = List.Item;
+        const Brief = Item.Brief;
+        const userId = this.props.user._id;
         return(
             <div>
-                <h2>消息列表</h2>
+                    {chatList.map(v=>{
+                        const lastItem = this.getLast(v);
+                        const targetId = v[0].from===userId ? v[0].to : v[0].from; 
+                        const name = this.props.chat.users[targetId] ? this.props.chat.users[targetId].name : '';
+                        const avatar = this.props.chat.users[targetId] ? this.props.chat.users[targetId].avatar : '';
+                        return (
+                            <List key={lastItem._id}>
+                                <Item                                    
+                                    thumb={require(`../img/${avatar}.png`)}
+                                >
+                                    {lastItem.content}
+                                    <Brief>{name}</Brief>
+                                </Item>
+                            </List>
+                            
+                        )
+                        })}
             </div>
         )
     }
